b69b712a86e75c37bdbf7302f64b41fe4fc84b34
Niraj Mohabey <mohabey.work@gmail.com>
2024-12-01 22:38:35 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import Projects from "./components/Projects";
+import Skills from "./components/Skills";
+import Experience from "./components/Experience";
+import Education from "./components/Education";
+import Contact from "./components/Contact";
+
+const App = () => {
+  return (
+    <main className="mx-auto max-w-7xl overflow-x-hidden antialiased">
+      <div className="bg-image fixed inset-0 bg-cover bg-fixed bg-center"></div>
+      <div className="relative z-10">
+        <Navbar />
+        <Hero />
+        <Projects />
+        <Skills />
+        <Experience />
+        <Contact />
+      </div>
+    </main>
+  );
+};
+
+export default App;
