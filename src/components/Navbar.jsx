2d61fac083903f6dbae0cfc43888adf1ce4fb0f4
Niraj Mohabey <mohabey.work@gmail.com>
2025-01-02 00:40:34 -0500

    Navbar 2

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -13,7 +13,7 @@ const Navbar = () => {
     e.preventDefault();
     const targetElement = document.querySelector(href);
     if (targetElement) {
-      const offset = -85; // Adjust this value for offset
+      const offset = -50; // Adjusted offset for smaller navbar
       const elementPosition = targetElement.getBoundingClientRect().top;
       const offsetPosition = elementPosition + window.scrollY + offset;
 
@@ -30,21 +30,23 @@ const Navbar = () => {
       <nav className="fixed left-0 right-0 z-50 lg:top-4">
         {/* Desktop Menu */}
         <div
-          className="mx-auto hidden w-[90%] max-w-xl items-center justify-center
-            rounded-full border border-white/30 py-2 backdrop-blur-lg lg:flex"
+          className="mx-auto hidden max-w-md items-center justify-center
+            rounded-full border border-white/30 py-1 px-4 backdrop-blur-lg lg:flex"
         >
-          <div className="flex items-center justify-between gap-6">
+          <div className="flex items-center justify-between gap-4">
             <div>
               <a href="/">
-                <span className="uppercase">Niraj Mohabey</span>
+                <span className="uppercase font-bold text-base">
+                  Niraj Mohabey
+                </span>
               </a>
             </div>
             <div>
-              <ul className="flex items-center gap-4">
+              <ul className="flex items-center gap-3">
                 {NAVIGATION_LINKS.map((item, index) => (
                   <li key={index}>
                     <a
-                      className="text-sm hover:text-stone-300"
+                      className="hover:text-stone-300 text-sm"
                       href={item.href}
                       onClick={(e) => handleLinkClick(e, item.href)}
                     >
@@ -57,11 +59,11 @@ const Navbar = () => {
           </div>
         </div>
         {/* Mobile Menu */}
-        <div className="py-2 backdrop-blur-md lg:hidden">
+        <div className="py-1 px-3 backdrop-blur-md lg:hidden">
           <div className="flex items-center justify-between">
             <div>
               <a href="#">
-                <span className="pl-2 uppercase">Niraj Mohabey</span>
+                <span className="pl-2 uppercase text-base">Niraj Mohabey</span>
               </a>
             </div>
             <div className="flex items-center">
@@ -71,20 +73,20 @@ const Navbar = () => {
                 aria-label={isMobileMenuOpen ? "Close Menu" : "Open Menu"}
               >
                 {isMobileMenuOpen ? (
-                  <RiCloseLine className="m-2 h-6 w-5" />
+                  <RiCloseLine className="m-1 h-6 w-6" />
                 ) : (
-                  <RiMenu3Line className="m-2 h-6 w-5" />
+                  <RiMenu3Line className="m-1 h-6 w-6" />
                 )}
               </button>
             </div>
           </div>
           {isMobileMenuOpen && (
-            <ul className="my-4 ml-4 flex flex-col gap-6 backdrop-blur-md">
+            <ul className="my-3 ml-4 flex flex-col gap-4 backdrop-blur-md">
               {NAVIGATION_LINKS.map((item, index) => (
                 <li key={index}>
                   <a
                     href={item.href}
-                    className="block w-full text-lg"
+                    className="block w-full text-sm"
                     onClick={(e) => handleLinkClick(e, item.href)}
                   >
                     {item.label}
