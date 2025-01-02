f3f2d7184dc796c6ca0705892cc4f6f8c1f7dff6
Niraj Mohabey <mohabey.work@gmail.com>
2025-01-02 00:33:31 -0500

    Nav bar adjustments 1

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,14 +4,16 @@ import { RiCloseLine, RiMenu3Line } from "@remixicon/react";
 
 const Navbar = () => {
   const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+
   const toggleMobileMenu = () => {
     setIsMobileMenuOpen(!isMobileMenuOpen);
   };
+
   const handleLinkClick = (e, href) => {
     e.preventDefault();
     const targetElement = document.querySelector(href);
     if (targetElement) {
-      const offset = -85;
+      const offset = -85; // Adjust this value for offset
       const elementPosition = targetElement.getBoundingClientRect().top;
       const offsetPosition = elementPosition + window.scrollY + offset;
 
@@ -22,12 +24,13 @@ const Navbar = () => {
     }
     setIsMobileMenuOpen(false);
   };
+
   return (
     <div>
       <nav className="fixed left-0 right-0 z-50 lg:top-4">
         {/* Desktop Menu */}
         <div
-          className="mx-auto hidden max-w-xl items-center justify-center
+          className="mx-auto hidden w-[90%] max-w-xl items-center justify-center
             rounded-full border border-white/30 py-2 backdrop-blur-lg lg:flex"
         >
           <div className="flex items-center justify-between gap-6">
